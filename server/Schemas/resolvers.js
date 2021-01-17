28e6cf6c4fd17f73d8e0c1fc2b995d1c2decab3c
John George <68780394+johngeorge88@users.noreply.github.com>
2021-01-17 14:19:06 -0500

    fixing the delete book mutation

--- server/Schemas/resolvers.js
+++ server/Schemas/resolvers.js
@@ -48,7 +48,7 @@ const resolvers = {
             }
             throw new AuthenticationError('You need to be logged in!')
         },
-        deleteBook: async (parent, { bookId }, context) => {
+        removeBook: async (parent, { bookId }, context) => {
             if (context.user) {
                 const updatedUser = await User.findOneAndUpdate(
                     { _id: context.user._id },
